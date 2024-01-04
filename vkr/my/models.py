2d2f1b4ef705c30626266b0ea4edf5ff320068dd
Marukhlenko A.L. <gproxygen@gmail.com>
2024-01-04 15:35:26 +0300

    easy views + style

--- vkr/my/models.py
+++ vkr/my/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 
 class Account(models.Model):
-    login = models.CharField(max_length=25, unique=True, verbose_name='логин')
-    passw = models.CharField(max_length=25, validators=[MinLengthValidator(3)], verbose_name='пароль')    
-    busy = models.BooleanField(verbose_name='выдана', null=True, default=False)
-    fio = models.CharField(max_length=255, null=True, verbose_name='ФИО')
-    phone = models.CharField(max_length=20, null=True, verbose_name='контактный телефон')
-    org = models.CharField(max_length=255, null=True, verbose_name='название организации')
-    region = models.CharField(max_length=255, null=True, default='Курск',verbose_name='область (город)')
-    comment = models.CharField(max_length=255, null=True, verbose_name='комментарий')
+    login = models.CharField('логин', max_length=25, unique=True)
+    passw = models.CharField('пароль', max_length=25, validators=[MinLengthValidator(3)])    
+    busy = models.BooleanField('выдана', default=False)
+    fio = models.CharField('ФИО',max_length=255, null=True)
+    phone = models.CharField('телефон',max_length=20, null=True)
+    org = models.CharField('организация',max_length=255, null=True)
+    region = models.CharField('область (город)', max_length=255, null=True, default='Курск')
+    comment = models.CharField('комментарий',max_length=255, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     class Meta :
@@ -19,8 +19,8 @@ class Account(models.Model):
         ordering = ['-updated_at']
 
 class Test(models.Model):
-    info = models.CharField(verbose_name='информация', max_length=255)
-    enable = models.BooleanField(verbose_name='доступна', null=True, default=False)
+    info = models.CharField('информация', max_length=255)
+    enable = models.BooleanField('доступно?', default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)    
     class Meta :
