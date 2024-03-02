8961454d9c8e293b548a0a01f3ec3a5d7006ea2b
Марухленко Анатолий <proxy33@mail.ru>
2024-03-02 14:33:39 +0300

    добавлена страница "О нас" с лампочкой

--- 00001/AR/app/views.py
+++ 00001/AR/app/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Это тестовая страница нашей выпускной работы!!!")
+
+def about(request):
+    return render(request,'app/about.html',{'title':'О нас'}) 
