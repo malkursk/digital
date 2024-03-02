b568b0cfe1c56f7ec23b202131cfd794c062b261
Марухленко Анатолий <proxy33@mail.ru>
2024-03-02 16:48:27 +0300

    вывод списка новостей, карточки новости

--- 00001/AR/app/urls.py
+++ 00001/AR/app/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.about, name="about"),
+    path("", views.cards, name="cards"),
+    # path('create',  create, name='url_create'),  
+    path('preview/<id>',  views.preview),       
+    # path('update/<id>',  update),   
+    # path('delete/<int:id>',  destroy),     
+    path("about", views.about, name="about"),
 ]
--- 00001/AR/app/views.py
+++ 00001/AR/app/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from app.models import News
+from django.core import serializers
+import json
+
 
 def index(request):
     return HttpResponse("Это тестовая страница нашей выпускной работы!!!")
 
 def about(request):
     return render(request,'app/about.html',{'title':'О нас'}) 
+
+def cards(request):
+    return render(request,'app/cards.html',{'data':News.objects.all()}) 
+
+def preview(request, id):
+    data = serializers.serialize("json", [News.objects.get(id=id)] )[1:-1]
+    data = json.loads(data)
+    return render(request, 'app/element.html', {'form': data, 'title':'Просмотр, id:' + id})    
