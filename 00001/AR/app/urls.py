cacc8bcb66164999441d9b2ef7b6fa056902f688
Marukhlenko A.L. <gproxygen@gmail.com>
2024-03-04 18:25:17 +0300

    added search + secret key in headers

--- 00001/AR/app/urls.py
+++ 00001/AR/app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.cards_page, name="cards"),
+    path("", views.cards, name="cards"),
     path('create',  views.create, name='create'),  
     path('preview/<id>',  views.preview, name="preview"),       
     path('update/<id>',  views.update,name="update"),   
--- 00001/AR/app/views.py
+++ 00001/AR/app/views.py
@@ -5,6 +5,7 @@ from app.models import News
 from django.core import serializers
 import json
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 
 def index(request):
@@ -18,12 +19,16 @@ def preview(request, id):
     data = json.loads(data)
     return render(request, 'app/element.html', {'form': data, 'title':'Просмотр, id:' + id})    
 
-def cards_page(request):
-    contact_list = News.objects.all()
-    paginator = Paginator(contact_list, 10)
+def cards(request):
+    query = request.GET.get("search")
+    if (query):
+        list = News.objects.filter(Q(caption__iregex=query) | Q(annotation__iregex=query))
+    else:
+        list = News.objects.all()    
+    paginator = Paginator(list, 10)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
-    return render(request, "app/cards.html", {"data": page_obj})
+    return render(request, "app/cards.html", {"data": page_obj,"search":query})
 
 def update(request, id):
     data = News.objects.get(id=id)  
