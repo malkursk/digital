fb2e92c96a359905e3dbdfbced3c7692e79b540f
Чаплыгина Ольга <@example.com>
2024-03-05 18:40:52 +0300

    Сушков Даниил пр9

--- 12682/X/my/models.py
+++ 12682/X/my/models.py
@@ -3,7 +3,10 @@ from django.db import models
 class News(models.Model):
     caption = models.CharField('заголовок новости', max_length=100)
     full_text = models.TextField('полный текст новости')
-    is_public = models.BooleanField('для открытой печати', default='ДА')
+    last_name = models.CharField('фамилия', max_length=30)
+    address = models.CharField('адрес', max_length=60)
+    phone_number = models.CharField('номер телефона пользователя', max_length=12)
+    is_public = models.BooleanField('для открытой печати', default='False')
     updated_at = models.DateTimeField(auto_now=True)    
     class Meta :
         db_table = "news"
